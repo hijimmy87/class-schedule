afd55ef2509e2d342dc8e6ba0b95b6a72d176e93
hijimmy87 <70084604+hijimmy87@users.noreply.github.com>
2022-08-25 23:41:26 +0800

    update

--- main.js
+++ main.js
@@ -1,48 +1,45 @@
-const table = document.getElementById('table'),
-    select = document.getElementById('select');
-
 function changeSchedule(cls) {
-    table.children[2].innerHTML = '';
-    if (schedule[cls] === undefined) {
-        table.children[1].classList.add('visually-hidden');
-        table.children[0].innerText = 'Not Found';
-        return;
+  const table = document.getElementById('table');
+  table.children[2].innerHTML = '';
+  if (schedule[cls] === undefined) {
+    table.children[0].innerText = 'Not Found';
+    table.children[1].setAttribute('hidden', '');
+    return;
+  }
+  table.children[0].innerText = `Class ${cls}`;
+  table.children[1].removeAttribute('hidden');
+  for (let i = 0; i < schedule[cls].length; i++) {
+    let tr = document.createElement('tr'),
+      th = document.createElement('th');
+    table.children[2].appendChild(tr);
+    tr.appendChild(th);
+    th.innerText = i + 1;
+    th.classList.add('fs-4');
+    for (let j = 0; j < 5; j++) {
+      let c = schedule[cls][i][j];
+      let td = document.createElement('td');
+      tr.appendChild(td);
+      if (c.length)
+        td.innerHTML = `<div class="fs-4">${c[0]}</div><div>${c[1]}</div>`;
     }
-    table.children[0].innerText = `Class ${cls}`;
-    table.children[1].classList.remove('visually-hidden');
-    for (let i = 0; i < schedule[cls].length; i++) {
-        let tr = document.createElement('tr'),
-            th = document.createElement('th');
-        table.children[2].appendChild(tr);
-        tr.appendChild(th);
-        th.innerText = i + 1;
-        th.classList.add('fs-4');
-        for (let j = 0; j < 5; j++) {
-            let c = schedule[cls][i][j];
-            let td = document.createElement('td');
-            tr.appendChild(td);
-            if (c.length) {
-                td.innerHTML = `<div class="fs-4">${c[0]}</div><div>${c[1]}</div>`;
-            }
-        }
-    }
+  }
 }
 
-select.onchange = () => {
-    changeSchedule(select.selectedOptions[0].value)
-};
-
 (() => {
-    for (const key in schedule) {
-        let opt = document.createElement('option');
-        opt.value = key;
-        opt.innerText = key;
-        if (key == '306') opt.selected = true;
-        select.appendChild(opt);
-    }
+  const select = document.getElementById('select');
+  for (const key in schedule) {
     let opt = document.createElement('option');
-    opt.value = 'TST';
-    opt.innerText = 'Test';
+    opt.value = key;
+    opt.innerText = key;
+    if (key == '306') opt.selected = true;
     select.appendChild(opt);
-    changeSchedule('306');
+  }
+  let opt = document.createElement('option');
+  opt.value = 'TST';
+  opt.innerText = 'Test';
+  select.appendChild(opt);
+  select.onchange = () => {
+    changeSchedule(select.selectedOptions[0].value)
+  };
+  changeSchedule('306');
 })();
