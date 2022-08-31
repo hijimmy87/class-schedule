1b3129d9ca8bac247cc6196182bc8e6133ba1ce0
hijimmy87 <70084604+hijimmy87@users.noreply.github.com>
2022-09-01 00:21:23 +0800

    indent

--- crawl.py
+++ crawl.py
@@ -8,7 +8,7 @@ CNUM = '零一二三四五六七八九十'
 
 url = 'http://web.hshs.ntpc.edu.tw/pkvsweb/faclas2.asp'
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
 }
 
 
@@ -16,51 +16,51 @@ schedules = {}
 cs = set()
 
 for grade in [3]:
-    for group in range(1, 11):
-        cls = f'{grade}{group:02d}'
-        classname = f'{CNUM[grade]}年{CNUM[group]}班'
-        print(classname)
+  for group in range(1, 11):
+    cls = f'{grade}{group:02d}'
+    classname = f'{CNUM[grade]}年{CNUM[group]}班'
+    print(classname)
 
-        data = {'tname': classname.encode('big5')}
-        req = requests.post(url, data=data, headers=headers)
-        soup = BeautifulSoup(req.content, 'lxml')
+    data = {'tname': classname.encode('big5')}
+    req = requests.post(url, data=data, headers=headers)
+    soup = BeautifulSoup(req.content, 'lxml')
 
-        schedules[cls] = []
-        trs = soup.find_all('tr')
+    schedules[cls] = []
+    trs = soup.find_all('tr')
 
-        for i in range(2, len(trs), 2):
-            classes = trs[i].find_all('td')
-            teachers = trs[i+1].find_all('td')
-            schedules[cls].append([])
-            for j in range(1, 6):
-                c = classes[j].text
-                t = teachers[j].text
-                if c == '.':
-                    c = ''
-                if t == '.':
-                    t = ''
-                cs.update((c, t))
-                schedules[cls][-1].append([c, t] if c and t else [])
-        time.sleep(1)
+    for i in range(2, len(trs), 2):
+      classes = trs[i].find_all('td')
+      teachers = trs[i+1].find_all('td')
+      schedules[cls].append([])
+      for j in range(1, 6):
+        c = classes[j].text
+        t = teachers[j].text
+        if c == '.':
+          c = ''
+        if t == '.':
+          t = ''
+        cs.update((c, t))
+        schedules[cls][-1].append([c, t] if c and t else [])
+    time.sleep(1)
 
 cs = list(cs)
 random.shuffle(cs)
 
 for cls in schedules.keys():
-    if cls == 'cs':
-        continue
-    for i in range(len(schedules[cls])):
-        for j in range(len(schedules[cls][i])):
-            if len(schedules[cls][i][j]) == 0:
-                c, t = 0, 0
-            else:
-                c = cs.index(schedules[cls][i][j][0])
-                t = cs.index(schedules[cls][i][j][1])
-            schedules[cls][i][j] = (c << 8)+t
+  if cls == 'cs':
+      continue
+  for i in range(len(schedules[cls])):
+    for j in range(len(schedules[cls][i])):
+      if len(schedules[cls][i][j]) == 0:
+        c, t = 0, 0
+      else:
+        c = cs.index(schedules[cls][i][j][0])
+        t = cs.index(schedules[cls][i][j][1])
+      schedules[cls][i][j] = (c << 8)+t
 
 with open('schedule.js', 'w', encoding='utf-8') as file:
-    file.write('const cs=')
-    json.dump(cs, file, ensure_ascii=False)
-    file.write(',sc=')
-    json.dump(schedules, file, ensure_ascii=False)
-    file.write(';')
+  file.write('const cs=')
+  json.dump(cs, file, ensure_ascii=False)
+  file.write(',sc=')
+  json.dump(schedules, file, ensure_ascii=False)
+  file.write(';')
--- main.js
+++ main.js
@@ -1,54 +1,54 @@
 const change = cls => {
-    if (Math.random() < 0.09487) {
-        setTimeout(() => {
-            let a = document.createElement('a');
-            a.href = 'https://youtu.be/jNQXAC9IVRw';
-            a.click();
-        }, 1487);
+  if (Math.random() < 0.0487) {
+    setTimeout(() => {
+      let a = document.createElement('a');
+      a.href = 'https://youtu.be/jNQXAC9IVRw';
+      a.click();
+    }, 1487);
+  }
+  const table = document.getElementById('table');
+  table.children[2].innerHTML = '';
+  if (sc[cls] === undefined) {
+    table.children[0].innerText = 'Not Found';
+    table.children[1].setAttribute('hidden', '');
+    return;
+  }
+  table.children[0].innerText = `Class ${cls}`;
+  table.children[1].removeAttribute('hidden');
+  for (let i = 0; i < sc[cls].length; i++) {
+    let tr = document.createElement('tr'),
+      th = document.createElement('th');
+    table.children[2].appendChild(tr);
+    tr.appendChild(th);
+    th.innerText = i + 1;
+    th.classList.add('fs-4');
+    for (let j = 0; j < sc[cls][i].length; j++) {
+      let td = document.createElement('td');
+      tr.appendChild(td);
+      if (sc[cls][i][j])
+        td.innerHTML = `<div class="fs-4">${cs[sc[cls][i][j] >> 8]}</div><div>${cs[sc[cls][i][j] & 255]}</div>`;
     }
-    const table = document.getElementById('table');
-    table.children[2].innerHTML = '';
-    if (sc[cls] === undefined) {
-        table.children[0].innerText = 'Not Found';
-        table.children[1].setAttribute('hidden', '');
-        return;
-    }
-    table.children[0].innerText = `Class ${cls}`;
-    table.children[1].removeAttribute('hidden');
-    for (let i = 0; i < sc[cls].length; i++) {
-        let tr = document.createElement('tr'),
-            th = document.createElement('th');
-        table.children[2].appendChild(tr);
-        tr.appendChild(th);
-        th.innerText = i + 1;
-        th.classList.add('fs-4');
-        for (let j = 0; j < sc[cls][i].length; j++) {
-            let td = document.createElement('td');
-            tr.appendChild(td);
-            if (sc[cls][i][j])
-                td.innerHTML = `<div class="fs-4">${cs[sc[cls][i][j] >> 8]}</div><div>${cs[sc[cls][i][j] & 255]}</div>`;
-        }
-    }
+  }
 };
 
 (() => {
-    const selected = '306',
-        select = document.getElementById('select');
-    for (const cls in sc) {
-        let opt = document.createElement('option');
-        select.appendChild(opt);
-        opt.value = cls;
-        opt.innerText = cls;
-        if (cls == selected) {
-            opt.selected = true;
-            change(cls);
-        }
-    }
+  const selected = '306',
+    select = document.getElementById('select');
+  for (const cls in sc) {
     let opt = document.createElement('option');
-    opt.value = 'TST';
-    opt.innerText = 'Test';
     select.appendChild(opt);
-    select.onchange = () => {
-        change(select.selectedOptions[0].value)
-    };
+    opt.value = cls;
+    opt.innerText = cls;
+    if (cls == selected) {
+      opt.selected = true;
+      change(cls);
+    }
+  }
+  let opt = document.createElement('option');
+  opt.value = 'TST';
+  opt.innerText = 'Test';
+  select.appendChild(opt);
+  select.onchange = () => {
+    change(select.selectedOptions[0].value)
+  };
 })();
